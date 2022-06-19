c3e128304970e30cef49387807b6da8ca34bd376
Tangilur Rahman <tangilurrahaman@gmail.com>
2022-06-20 03:18:00 +0600

    done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+
+const App = () => {
+  return (
+    <>
+    <div className="container">
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem perferendis maxime quam optio voluptatibus culpa amet possimus fuga eaque a!
+    </div>
+    
+    </>
+  )
+}
+
+export default App
