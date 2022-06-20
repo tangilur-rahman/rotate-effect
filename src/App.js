88bcf82289d54cc6c34a387884965b5e06186325
Tangilur Rahman <tangilurrahaman@gmail.com>
2022-06-20 11:24:39 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import "./App.css"
+import "./App.css";
 
 const App = () => {
-  return (
-    <>
-    <div className="container">
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem perferendis maxime quam optio voluptatibus culpa amet possimus fuga eaque a!
-    </div>
-    
-    </>
-  )
-}
+	return (
+		<>
+			<div className="container">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+				perferendis maxime quam optio voluptatibus culpa amet possimus fuga
+				eaque a!
+			</div>
+		</>
+	);
+};
 
-export default App
+export default App;
